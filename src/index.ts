9bdfa67ee5b1d300e78018a16a8045af5fa3efd8
sggnology <sggnology@gmail.com>
2025-03-15 18:11:19 +0900

    [master] 챗봇과 유사한 형태의 UI 제작

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,34 @@
-export interface Env {
-	// If you set another name in the Wrangler config file as the value for 'binding',
-	// replace "AI" with the variable name you defined.
-	AI: Ai;
-  }
-  
-  export default {
-	async fetch(request, env): Promise<Response> {
-	  const response = await env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
-		prompt: "What is the origin of the phrase Hello, World",
-	  });
-  
-	  return new Response(JSON.stringify(response));
-	},
-  } satisfies ExportedHandler<Env>;
+import { Hono } from 'hono'
+
+const app = new Hono<{ Bindings: Env }>()
+
+app.get('/', async (c) => {
+	const html = await c.env.ASSETS.fetch(new Request('index.html')).then(res => res.text());
+	return c.html(html);
+})
+
+interface SimpleQueryParams {
+	keyword: string
+}
+
+app.post('/api/normal', async (c) => {
+
+	// get body params
+	const bodyParams: SimpleQueryParams = await c.req.json();
+
+	// if keyword is empty throw
+	if (!bodyParams.keyword) {
+		return c.json({ error: 'Keyword cannot be empty' }, 400);
+	}
+
+	const result = await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+		messages: [
+			{ role: "system", content: "마케팅 전문가로 블로그를 담당하고 있는 전문가" },
+			{ role: "user", content: bodyParams.keyword },
+			{ role: "assistant", content: "답변 글자수 460자, 제목과 내용에 대한 중제목 소제목 그리고 내용 자체는 요약해서" },
+		]
+	})
+	return c.json(result);
+})
+
+export default app
